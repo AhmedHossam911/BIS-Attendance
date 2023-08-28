83ebcbdc5f268978cda1989d19a9824ea3a73d88
AHMED HOSSAM <124696607+AhmedHossam911@users.noreply.github.com>
2023-08-29 00:03:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,126 +1,351 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BIS Student</title>
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <nav class="navbar">
-        <div class="logo">
-            <img src="Helwan Logo copy.png" alt="University Logo">
-            <span>Helwan University Faculty Of Commerce & Business Administration</span>
-        </div>
-        <div class="logo">
-            <img src="Logo.png" alt="College Logo">
-            <span>Business Information Systems (BIS)</span>
-        </div>
-
-        <div class="time-widget">
-            <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=en&size=small&timezone=Africa%2FCairo" width="100%" height="90" frameborder="0" seamless></iframe>
-        </div>
-
-    </nav>
+// popups
+{
+  document.addEventListener("DOMContentLoaded", function() {
+      const openAnnouncementButton = document.getElementById("Lec1button");
+      const openCoursesScheduleButton = document.getElementById("Lec2button");
+      const openYourInfoButton = document.getElementById("Lec3button");
+      const openMedicalCareButton = document.getElementById("Lec4button");
     
-
-    <section class="student-section">
-      <div class="open-popup-buttons">
-
-          <button id="Lec1button" class="open-popup-button" onclick="generateAndStoreRandomCode()">Lecture 1
-            <img class="buttonIMG" src="evaluation.png" alt="cerfificate">
-          </button>
-
-          <button id="Lec2button" class="open-popup-button" onclick="generateAndStoreRandomCode()">Lecture 2
-            <img class="buttonIMG" src="evaluation.png" alt="cerfificate">
-          </button>
-
-          <button id="Lec3button" class="open-popup-button" onclick="generateAndStoreRandomCode()">Lecture 3
-            <img class="buttonIMG" src="evaluation.png" alt="cerfificate">
-          </button>
-
-          <button id="Lec4button" class="open-popup-button" onclick="generateAndStoreRandomCode()">Lecture 4
-            <img class="buttonIMG" src="evaluation.png" alt="cerfificate">
-          </button>
-
-
-        </div>
-  </section>
-
-<div id="popup_lec1" class="popup">
-    <div class="popup-content">
-        <span class="close-button" id="close-button">&times;</span>
-        <h2 style="color: red;">Lecture 1</h2>
-        
-        <p style="font-weight: bold; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">Lecture Code: <span id="random-code"></span></p>
-
-        <div>
-          <iframe class="Form" width="640px" height="480px"  src="https://forms.office.com/Pages/ResponsePage.aspx?id=alg1e43RXECOKVcThik3qd3pNTA3nkxAlPb7Z7EXaQFUREVBR1dGMjJLR0FEQktYNTFWVzAwT0hPUi4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
-        </div>
-    </div>
-</div>
-
-<div id="popup_lec2" class="popup">
-    <div class="popup-content">
-        <span class="close-button" id="close-button">&times;</span>
-        <h2 style="color: red;">Lecture 2</h2>
-        
-        <p style="font-weight: bold; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">Lecture Code: <span id="random-code-1"></span></p>
-
-        <div>
-          <iframe width="640px" height="480px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=alg1e43RXECOKVcThik3qS7uOS1Zc-5EvLCKlQeqIO5UNkRTNzRITlBRMjFGTTdWT0JGOUs1R0RRMS4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
-        </div>
-    </div>
-</div>  
-
-<div id="popup_lec3" class="popup">
-    <div class="popup-content">
-        <span class="close-button" id="close-button">&times;</span>
-        <h2 style="color: red;">Lecture 3</h2>
-        
-        <p style="font-weight: bold; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">Lecture Code: <span id="random-code-2"></span></p>
-
-        <div>
-          <iframe width="640px" height="480px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=alg1e43RXECOKVcThik3qd3pNTA3nkxAlPb7Z7EXaQFUMDRRNzFMU0pKUTI3MEg5SDc5VFJGTzM0MC4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>        </div>
-    </div>
-</div>
-
-<div id="popup_lec4" class="popup">
-    <div class="popup-content">
-        <span class="close-button" id="close-button">&times;</span>
-        <h2 style="color: red;">Lecture 4</h2>
-        
-        <p style="font-weight: bold; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">Lecture Code: <span id="random-code-3"></span></p>
-
-        <div>
-          <iframe width="640px" height="480px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=alg1e43RXECOKVcThik3qS7uOS1Zc-5EvLCKlQeqIO5UME1EWEk4MDlTTjFDVUNOTDdKNVc5REdTUC4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>    </div>
-  </div>
-  </div>
-
-<!-- Footer -->
-<footer>
-    <div class="footer-content">
-      <div class="footer-section">
-        <img src="fb.png" alt="Logo 1">
-        <a href="https://www.facebook.com/BIS.FCBA.Helwan.uni">Facebook</a>
-      </div>
-      <div class="footer-section">
-        <img src="facebook-group-facebook-group-icon-png.png" alt="Logo 2">
-        <a href="https://www.facebook.com/groups/483323791827046">Facebook Group</a>
-      </div>
-      <div class="footer-section">
-        <img src="internet-web-icon-in-trendy-flat-style-isolated-on-white-background-website-symbol-for-your-website-design-logo-app-ui-illustration-free-vector.jpg" alt="Logo 3">
-        <a href="http://fcba.helwan.edu.eg/?fbclid=IwAR0j-OzzCcLZYOopsVdb20qZjHnQSwaYPbwM559rsuC6jn_Dftdse0RUILw">Faculty Website</a>
-      </div>
-    </div>
-    <div class="footer-bottom">
-      <p>&copy; 2023 Business Information Systems (BIS). All rights reserved.</p>
-    </div>
-  </footer>
+      const popupLec1 = document.getElementById("popup_lec1");
+      const popupLec2 = document.getElementById("popup_lec2");
+      const popupLec3 = document.getElementById("popup_lec3");
+      const popupLec4 = document.getElementById("popup_lec4");
     
-
-  <script src="script.js" defer></script>
+      openAnnouncementButton.addEventListener("click", function() {
+        popupLec1.style.display = "block";
+      });
+    
+      openCoursesScheduleButton.addEventListener("click", function() {
+        popupLec2.style.display = "block";
+      });
+    
+      openYourInfoButton.addEventListener("click", function() {
+        popupLec3.style.display = "block";
+      });
+    
+      openMedicalCareButton.addEventListener("click", function() {
+        popupLec4.style.display = "block";
+      });
+    
+      const closeButtons = document.querySelectorAll(".close-button");
+      closeButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+          const popup = this.closest(".popup");
+          if (popup) {
+            popup.style.display = "none";
+          }
+        });
+      });
+    
+      window.addEventListener("click", function(event) {
+        if (event.target.classList.contains("popup")) {
+          event.target.style.display = "none";
+        }
+      });
+    });
+  }
   
-</body>
-</html>
+  // Generate a random code based on the current date lec1
+{
+  const givenCodes = [
+    "1E7LC9E3C21",
+    "1L5EC2L9C7E",
+    "1C8LE6E4C2L",
+    "19E3C7L1ELC",
+    "1L2CE8ELC6E",
+    "1C4LE9E5L1C",
+    "1E7C3L8C9LE",
+    "1L6EC1E8C3C",
+    "1C5LE9E2L1E",
+    "1E4LC7C1LE9"
+  ];
+  
+  function generateRandomIndex(maxIndex) {
+    return Math.floor(Math.random() * maxIndex);
+  }
+
+  function generateRandomCode() {
+    const randomIndex = generateRandomIndex(givenCodes.length);
+    return givenCodes[randomIndex];
+  }
+
+  function setCookie(name, value, days) {
+    const expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + expirationDate.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  }
+
+  function getCookie(name) {
+    const cookieName = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+    for (let i = 0; i < cookieArray.length; i++) {
+      let cookie = cookieArray[i];
+      while (cookie.charAt(0) === ' ') {
+        cookie = cookie.substring(1);
+      }
+      if (cookie.indexOf(cookieName) === 0) {
+        return cookie.substring(cookieName.length, cookie.length);
+      }
+    }
+    return "";
+  }
+
+  function updateRandomCode() {
+    const codeElement = document.getElementById('random-code');
+    if (codeElement) {
+      const storedRandomCode = getCookie('staticRandomCode');
+      if (!storedRandomCode) {
+        const randomCode = generateRandomCode();
+        setCookie('staticRandomCode', randomCode, 1); // Expires in 1 day
+        setCookie('staticRandomCodeTime', Date.now().toString(), 1); // Expires in 1 day
+        codeElement.textContent = randomCode;
+      } else {
+        const storedTime = parseInt(getCookie('staticRandomCodeTime'));
+        const currentTime = Date.now();
+        
+        // Keep the same code for 3 minutes (180000 milliseconds)
+        if (currentTime - storedTime >= 180000) {
+          const randomCode = generateRandomCode();
+          setCookie('staticRandomCode', randomCode, 1); // Expires in 1 day
+          setCookie('staticRandomCodeTime', currentTime.toString(), 1); // Expires in 1 day
+          codeElement.textContent = randomCode;
+        } else {
+          codeElement.textContent = storedRandomCode;
+        }
+      }
+    }
+  }
+
+  updateRandomCode();
+}
+                      
+// Generate a random code based on the current date lec2
+  {
+    const givenCodes = [
+      " 22E9C23L7C1 ",
+      " 22E5L7C2CE9 ", 
+      " 22L8E4C6E2C ",
+      " 22C7L3NE9E1 ",
+      " 22E8C6E2LC7 ",
+      " 22L4CE5E9C1 ",
+      " 22C7E9L3CL8 ",
+      " 22C6E1E8LC3 ",
+      " 22C5E1LE9E2 ",
+      " 22LE7C1CE94 ",
+    ];
+    
+    function generateRandomIndex(maxIndex) {
+      return Math.floor(Math.random() * maxIndex);
+    }
+  
+    function generateRandomCode() {
+      const randomIndex = generateRandomIndex(givenCodes.length);
+      return givenCodes[randomIndex];
+    }
+  
+    function setCookie(name, value, days) {
+      const expirationDate = new Date();
+      expirationDate.setTime(expirationDate.getTime() + (days * 24 * 60 * 60 * 1000));
+      const expires = "expires=" + expirationDate.toUTCString();
+      document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+  
+    function getCookie(name) {
+      const cookieName = name + "=";
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const cookieArray = decodedCookie.split(';');
+      for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+          cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(cookieName) === 0) {
+          return cookie.substring(cookieName.length, cookie.length);
+        }
+      }
+      return "";
+    }
+  
+    function updateRandomCode() {
+      const codeElement = document.getElementById('random-code-1');
+      if (codeElement) {
+        const storedRandomCode = getCookie('staticRandomCode');
+        if (!storedRandomCode) {
+          const randomCode = generateRandomCode();
+          setCookie('staticRandomCode', randomCode, 1); // Expires in 1 day
+          setCookie('staticRandomCodeTime', Date.now().toString(), 1); // Expires in 1 day
+          codeElement.textContent = randomCode;
+        } else {
+          const storedTime = parseInt(getCookie('staticRandomCodeTime'));
+          const currentTime = Date.now();
+          
+          // Keep the same code for 3 minutes (180000 milliseconds)
+          if (currentTime - storedTime >= 180000) {
+            const randomCode = generateRandomCode();
+            setCookie('staticRandomCode', randomCode, 1); // Expires in 1 day
+            setCookie('staticRandomCodeTime', currentTime.toString(), 1); // Expires in 1 day
+            codeElement.textContent = randomCode;
+          } else {
+            codeElement.textContent = storedRandomCode;
+          }
+        }
+      }
+    }
+  
+    updateRandomCode();
+  }
+                        
+  // Generate a random code based on the current date lec3
+    {
+      const givenCodes = [
+        "32E9C7L23C1" ,
+        "32E5LC7E29C" ,
+        "32L4E6C8E2C" ,
+        "32E1C7NL39E" ,
+        "32E8C6E72LC" ,
+        "32C4E9E15CL" ,
+        "32L3C79CE8L" , 
+        "32L1E3C86EC" ,
+        "32E9LC5E21E" ,
+        "32C1E7CE94L" ,
+      ];
+      
+      function generateRandomIndex(maxIndex) {
+        return Math.floor(Math.random() * maxIndex);
+      }
+    
+      function generateRandomCode() {
+        const randomIndex = generateRandomIndex(givenCodes.length);
+        return givenCodes[randomIndex];
+      }
+    
+      function setCookie(name, value, days) {
+        const expirationDate = new Date();
+        expirationDate.setTime(expirationDate.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + expirationDate.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+      }
+    
+      function getCookie(name) {
+        const cookieName = name + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const cookieArray = decodedCookie.split(';');
+        for (let i = 0; i < cookieArray.length; i++) {
+          let cookie = cookieArray[i];
+          while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+          }
+          if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+          }
+        }
+        return "";
+      }
+    
+      function updateRandomCode() {
+        const codeElement = document.getElementById('random-code-2');
+        if (codeElement) {
+          const storedRandomCode = getCookie('staticRandomCode');
+          if (!storedRandomCode) {
+            const randomCode = generateRandomCode();
+            setCookie('staticRandomCode', randomCode, 1); // Expires in 1 day
+            setCookie('staticRandomCodeTime', Date.now().toString(), 1); // Expires in 1 day
+            codeElement.textContent = randomCode;
+          } else {
+            const storedTime = parseInt(getCookie('staticRandomCodeTime'));
+            const currentTime = Date.now();
+            
+            // Keep the same code for 3 minutes (180000 milliseconds)
+            if (currentTime - storedTime >= 180000) {
+              const randomCode = generateRandomCode();
+              setCookie('staticRandomCode', randomCode, 1); // Expires in 1 day
+              setCookie('staticRandomCodeTime', currentTime.toString(), 1); // Expires in 1 day
+              codeElement.textContent = randomCode;
+            } else {
+              codeElement.textContent = storedRandomCode;
+            }
+          }
+        }
+      }
+    
+      updateRandomCode();
+    }
+                        
+    // Generate a random code based on the current date lec4
+    {
+      const givenCodes = [
+        " 43E9C21LC7  ",
+        " 43E5LC72CE9 ",
+        " 43L6E8C4E2  ",
+        " 43E1CN7EL93 ",
+        " 43E8C7E26LC ",
+        " 43C5E1E94LC ",
+        " 43L3CE79CE8 ",
+        " 43L1E6C2E8C ",
+        " 43E9L5CE12  ",
+        " 43C8CE719EL ",
+      ];
+    
+      function generateRandomIndex(maxIndex) {
+        return Math.floor(Math.random() * maxIndex);
+      }
+    
+      function generateRandomCode() {
+        const randomIndex = generateRandomIndex(givenCodes.length);
+        return givenCodes[randomIndex];
+      }
+    
+      function setCookie(name, value, days) {
+        const expirationDate = new Date();
+        expirationDate.setTime(expirationDate.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + expirationDate.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+      }
+    
+      function getCookie(name) {
+        const cookieName = name + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const cookieArray = decodedCookie.split(';');
+        for (let i = 0; i < cookieArray.length; i++) {
+          let cookie = cookieArray[i];
+          while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+          }
+          if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+          }
+        }
+        return "";
+      }
+    
+      function updateRandomCode() {
+        const codeElement = document.getElementById('random-code-3');
+        if (codeElement) {
+          const storedRandomCode = getCookie('staticRandomCode');
+          if (!storedRandomCode) {
+            const randomCode = generateRandomCode();
+            setCookie('staticRandomCode', randomCode, 1); // Expires in 1 day
+            setCookie('staticRandomCodeTime', Date.now().toString(), 1); // Expires in 1 day
+            codeElement.textContent = randomCode;
+          } else {
+            const storedTime = parseInt(getCookie('staticRandomCodeTime'));
+            const currentTime = Date.now();
+            
+            // Keep the same code for 3 minutes (180000 milliseconds)
+            if (currentTime - storedTime >= 180000) {
+              const randomCode = generateRandomCode();
+              setCookie('staticRandomCode', randomCode, 1); // Expires in 1 day
+              setCookie('staticRandomCodeTime', currentTime.toString(), 1); // Expires in 1 day
+              codeElement.textContent = randomCode;
+            } else {
+              codeElement.textContent = storedRandomCode;
+            }
+          }
+        }
+      }
+    
+      updateRandomCode();
+    }
+                        
